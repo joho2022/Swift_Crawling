26b57dad17ec2152a4bae8ee3cbef17fe0e68238
joho2022 <josama1997@naver.com>
2024-07-11 02:25:25 +0900

    슬랙 주소 환경변수 개선

--- merge_and_send.sh
+++ merge_and_send.sh
@@ -3,7 +3,7 @@
 SCRAP_DIR="/home/kai/scrap"  # 스크랩 파일이 저장된 디렉토리
 MERGED_FILE="$SCRAP_DIR/merged.json"  # 병합된 JSON 파일 경로
 CURRENT_TIME=$(date +"%Y년 %m월 %d일 %H시...")  # 현재 시간 포맷
-SLACK_WEBHOOK_URL="https://hooks.slack.com/services/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"  # Slack Webhook URL
+SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL}  # Slack Webhook URL
 
 # 모든 JSON 파일 병합
 jq -s 'map(.text) | map(split("\n")) | add | unique | join("\n") | {"text": ($current_time + "\n" + .)}' --arg current_time "$CURRENT_TIME" $SCRAP_DIR/*.json > $MERGED_FILE
